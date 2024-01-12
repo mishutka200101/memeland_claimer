1b3c7ce2b98e1da102e3d11bccc23f79bbbe9017
nazavod <91217742+nazavod777@users.noreply.github.com>
2024-01-12 16:39:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ async def main() -> None:
         case 1:
             tasks: list = [
                 asyncio.create_task(coro=balance_parser(account_data=current_account,
-                                                       next(proxies_cycled) if proxies_cycled else None))
+                                                        proxy=next(proxies_cycled) if proxies_cycled else None))
                 for current_account in accounts_list
             ]
 
