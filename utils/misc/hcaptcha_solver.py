ea050e9fc359827fbb65517628bcef1a92b7c896
nazavod <91217742+nazavod777@users.noreply.github.com>
2024-01-14 14:27:24 +0300

    Update hcaptcha_solver.py

--- utils/misc/hcaptcha_solver.py
+++ utils/misc/hcaptcha_solver.py
@@ -42,7 +42,11 @@ class HCaptchaSolver:
 
         while True:
             try:
-                async with aiohttp.ClientSession() as session:
+                async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(
+                        ssl=False,
+                        verify_ssl=None,
+                        ttl_dns_cache=300
+                )) as session:
                     r: aiohttp.ClientResponse = await session.post(url='https://api.capmonster.cloud/getTaskResult',
                                                                    json={
                                                                        'clientKey': config.CAPMONSTER_API_KEY,
