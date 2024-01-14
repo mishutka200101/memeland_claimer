547ce47c6222c39d24f8f5ca10a23c9a3d1d002f
nazavod <91217742+nazavod777@users.noreply.github.com>
2024-01-14 14:27:45 +0300

    Update change_proxy_by_url.py

--- utils/change_proxy_by_url.py
+++ utils/change_proxy_by_url.py
@@ -4,7 +4,11 @@ import aiohttp
 
 
 async def change_proxy_by_url(private_key: str) -> None:
-    async with aiohttp.ClientSession() as client:
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(
+                        ssl=False,
+                        verify_ssl=None,
+                        ttl_dns_cache=300
+                )) as client:
         r: aiohttp.ClientResponse = await client.get(url=config.CHANGE_PROXY_URL)
 
         logger.info(f'{private_key} | Changed Proxy by URL status: {r.status}')
